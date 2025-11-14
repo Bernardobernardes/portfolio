b0da26536aff793ca86b2bf0641f05dd22e26a0b
Bernardo Bernardes <bernardobernardes826@gmail.com>
2025-11-13 22:37:26 -0300

    Primeiro commit

---
+++ Linguagens/Linguagens.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".toggle-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const desc = btn.nextElementSibling;
+    if (desc.style.display === "block") {
+      desc.style.display = "none";
+      btn.textContent = "Ver mais";
+    } else {
+      desc.style.display = "block";
+      btn.textContent = "Ver menos";
+    }
+  });
+});
